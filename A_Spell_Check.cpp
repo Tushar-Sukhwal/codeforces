5b5ed549ed6d11891bd624b843354f328415685a
Tushar Sukhwal <tushars7740@gmail.com>
2023-01-31 16:51:57 +0530

    commit

--- A_Spell_Check.cpp
+++ A_Spell_Check.cpp
@@ -9,13 +9,9 @@ using namespace std;
 #define mp make_pair
 #define endl "\n"
 #define ub  upper_bound // '>'
-#define lb  lower_bound // '>='
+#define lb  lower_bound // '<='
 const ll pi = 3.14159265358979323846;
-// const ll mod = 1000000007;
-// const ll mod = 998244353;
 // ll mod;
-
-
 // vectors
 // oops size(x), rbegin(x), rend(x) need C++17
 #define vi vector<int> 
@@ -30,54 +26,86 @@ const ll pi = 3.14159265358979323846;
 #define eb emplace_back
 #define ft front()
 #define bk back()
-
-// loops
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define F0R(i, a) FOR(i, 0, a)
 #define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
 #define R0F(i, a) ROF(i, 0, a)
 #define each(a, x) for (auto &a : x)
 #define fi for(int i = 0 ; i<n ; i++) 
-#define fj for(int i = 0 ; i<n ; i++) 
+#define fj for(int j = 0 ; j<n ; j++) 
+#define jaiShreeRam int main() 
+#define letsgooooooooooo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-/* stuff you should look for
+
+//ASCII values a=97 A=65
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+
+
+//! bitwise operators
+//&  and
+//|  or
+//^  XOR
+//<< left shift (*2)
+//>> right shift (/2)
+//~  bitwise not
+//__builtin_popcount(uli)  --> to calculate the no of set bits O(1)
+
+//! IMPORTANT FUNCTIONS 
+//1. binary_search(startaddress,endaddress, valuetofind) --> true or false
+
+
+/* //!stuff you should look for
  * int overflow, array bounds
  * special cases (n=1?)
  * do smth instead of nothing and stay organized
  * WRITE STUFF DOWN
  * DON'T GET STUCK ON ONE APPROACH
+ * Fcking use lli instead of int
+ * use map/set instead of unordered map/set whererver possible
  */
 
+//!sieve of eratosthenes 
+//TC = n*(log(log(n)) ; 
+/* void SieveOfEratosthenes(int n)
+{
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+ 
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
 
-
-
-
-
-
-#define letsgooooooooooo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-
-int main(){
-    
-    letsgooooooooooo
-    test{
-        lli n ; cin >> n ; string str ; cin >> str ; 
-
-        int flag = 1 ; 
-        for(int i = 0 ; i <n ; i++){
-            if(str)
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
         }
-
-
-
     }
 
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+} */
+
+//!to sort by the second element in pair 
+/* bool sortbysec(const pair<int,int> &a,
+              const pair<int,int> &b)
+{
+    return (a.second < b.second);
+} */
+//!to sort in descending order    
+// greater<int>()
+
+
+jaiShreeRam{
+    
+    letsgooooooooooo
+    
+
+    
+
     
 
 
-
-
-
+    
     return 0;
 }
 //<<<<<<<<<<Tushars_07>>>>>>>>>>>>>>>>
